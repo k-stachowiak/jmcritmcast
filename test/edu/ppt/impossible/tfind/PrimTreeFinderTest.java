b049f4f0f8617772d5815e4415ffa7aa34b8529b
Krzysztof Stachowiak <krzysztof@kdell.(none)>
2012-08-18 13:16:59 +0200

    Bug #1 missing test.

---
+++ test/edu/ppt/impossible/tfind/PrimTreeFinderTest.java
@@ -0,0 +1,44 @@
+package edu.ppt.impossible.tfind;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import edu.ppt.impossible.helpers.IndexMetricProvider;
+import edu.ppt.impossible.helpers.MetricProvider;
+import edu.ppt.impossible.helpers.TopologyAnalyser;
+import edu.ppt.impossible.helpers.TopologyAnalyserImpl;
+import edu.ppt.impossible.model.AdjacencyListFactory;
+import edu.ppt.impossible.model.Graph;
+import edu.ppt.impossible.model.GraphFactory;
+
+public class PrimTreeFinderTest {
+
+	@Test
+	public void testForCutsCheapestEdgesNPE() {
+
+		// Constants.
+		final int INDEX_FOR_METRIC_PROVIDER = 0;
+
+		// Helpers.
+		final GraphFactory graphFactory = new AdjacencyListFactory();
+		
+		final MetricProvider metricProvider = new IndexMetricProvider(
+				INDEX_FOR_METRIC_PROVIDER);
+		
+		final TopologyAnalyser topologyAnalyser = new TopologyAnalyserImpl(
+				new PrimTreeFinder(metricProvider));
+
+		// Input.
+		Graph graph = graphFactory.createTest();
+
+		// Case.
+		boolean expected = true;
+		boolean actual = topologyAnalyser.isConnected(graph);
+
+		// Assert.
+		assertEquals(expected, actual);
+	}
+
+}
