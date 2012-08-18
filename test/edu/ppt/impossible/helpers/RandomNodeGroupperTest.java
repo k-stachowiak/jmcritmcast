802fe1c444f6eda9d63cdc98b244bafe6de19415
Krzysztof Stachowiak <krzysztof@kdell.(none)>
2012-08-18 13:16:32 +0200

    Bug #2 Random node groupper throws inted out of bounds exception.

---
+++ test/edu/ppt/impossible/helpers/RandomNodeGroupperTest.java
@@ -0,0 +1,40 @@
+package edu.ppt.impossible.helpers;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.Random;
+
+import org.junit.Test;
+
+import edu.ppt.impossible.model.AdjacencyListFactory;
+import edu.ppt.impossible.model.Graph;
+import edu.ppt.impossible.model.GraphFactory;
+import edu.ppt.impossible.model.Node;
+
+public class RandomNodeGroupperTest {
+
+	@Test
+	public void testForGroupsAOOB() {
+
+		// Constants
+		final int GROUP_SIZE = 3;
+
+		// Helpers.
+		Random random = new Random(1);
+		NodeGroupper groupper = new RandomNodeGroupper(random);
+		GraphFactory graphFactory = new AdjacencyListFactory();
+
+		// Input.
+		Graph graph = graphFactory.createTest();
+
+		// Case.
+		List<Node> actualList = groupper.group(graph, GROUP_SIZE);
+		int actual = actualList.size();
+		int expected = GROUP_SIZE;
+
+		// Assertions.
+		assertEquals(expected, actual);
+	}
+
+}
